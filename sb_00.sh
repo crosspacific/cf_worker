ba433079bb0a81a3ed38087f7b2d12042e94127d
crosspacific <48462348+crosspacific@users.noreply.github.com>
2024-08-16 09:20:13 +0800

    Update sb_00.sh

--- sb_00.sh
+++ sb_00.sh
@@ -354,7 +354,7 @@ generate_config() {
         "format": "binary",
         "url": "https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-category-ads-all.srs",
         "download_detour": "direct"
-      },      
+      },
       {
         "tag": "geosite-category-porn",
         "type": "remote",
